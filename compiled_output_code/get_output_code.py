737fec8dda5295cf1a2c192cb023cd6a46f45398
Mark Saroufim <marksaroufim@meta.com>
2024-02-28 19:10:32 +0000

    update

--- compiled_output_code/get_output_code.py
+++ compiled_output_code/get_output_code.py
@@ -1,9 +1,15 @@
+# This has a few issues
+# 1. `optimizer.step()` and `optimizer.zero_grad()` are not supported in fullgraph mode
+# 2. `nn.MSELoss()` is not supported in fp16 on cpu
+# loss.backwards() not supported in fullgraph
+
 import torch
 import torch.nn as nn
 import torch.optim as optim
 import os
 
-torch.set_default_device("cuda")
+torch.set_default_device("cpu")
+torch.set_default_dtype(torch.float32)
 
 class SimpleNet(nn.Module):
     def __init__(self):
@@ -16,24 +22,27 @@ class SimpleNet(nn.Module):
 # Put these outside of main() otherwise torch.compile() craps out
 net = SimpleNet()
 optimizer = optim.Adam(net.parameters(), lr=0.001)
-criterion = nn.MSELoss()
+# criterion = nn.MSELoss() # Not supported in fp16 on cpu
+criterion = nn.L1Loss()
 
 def main(input):
     # Dummy input and target data
     # input = torch.randn(1, 10) 
-    target = torch.randn(1, 1)
-    output = net(input)
+    for _ in range(128):
+        target = torch.randn(1, 1)
+        output = net(input)
 
-    loss = criterion(output, target)
+        loss = criterion(output, target)
 
-    optimizer.zero_grad()
-    loss.backward()
+        # optimizer.zero_grad() - This graph greaks 
+        # loss.backward()
 
-    # Step 7: Single optimizer step
-    optimizer.step()
+        # Step 7: Single optimizer step
+        optimizer.step()
 
 
 if __name__ == "__main__":
-    main = torch.compile(main)
+    main = torch.compile(main, fullgraph=True)
+    
     main(torch.randn(1,10))
     # main()
