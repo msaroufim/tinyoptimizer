678d69e3ca6724aefe79bc23ec371f225cb87c7e
Mark Saroufim <marksaroufim@gmail.com>
2024-03-27 16:55:28 +0000

    bla

--- cpu_optimizer/pure_torch.py
+++ cpu_optimizer/pure_torch.py
@@ -1,3 +1,7 @@
+# m helps by keeping track of the direction that has been generally good for going downhill, filtering out some of the noise and missteps
+# v adjusts your stride. If the ground has been uneven recently (high variance), it tells you to take smaller steps to be safe
+# If the ground is smooth (low variance), you can confidently take bigger steps.
+
 # Python implementation
 class AdamOptimizer:
     def __init__(self, parameters, lr=0.001, beta1=0.9, beta2=0.999, epsilon=1e-8):
@@ -5,6 +9,8 @@ class AdamOptimizer:
         self.beta1 = beta1
         self.beta2 = beta2
         self.epsilon = epsilon
+        self.weight_decay = weight_decay
+
         self.m = [0.0 for _ in parameters]
         self.v = [0.0 for _ in parameters]
         self.t = 0
@@ -22,7 +28,7 @@ class AdamOptimizer:
             m_hat[i] = self.m[i] / (1 - self.beta1 ** self.t)
             v_hat[i] = self.v[i] / (1 - self.beta2 ** self.t)
 
-            self.parameters[i] -= self.lr * m_hat[i] / (v_hat[i] ** 0.5 + self.epsilon)
+            self.parameters[i] -= (self.lr * m_hat / (v_hat ** 0.5 + self.epsilon)) + (self.weight_decay * self.lr * param)
 
         return self.parameters
 
@@ -34,6 +40,8 @@ class AdamOptimizer:
         self.beta1 = beta1
         self.beta2 = beta2
         self.epsilon = epsilon
+        self.weight_decay = weight_decay
+
         self.m = [torch.zeros_like(p) for p in params]
         self.v = [torch.zeros_like(p) for p in params]
         self.t = 0
@@ -46,6 +54,8 @@ class AdamOptimizer:
                 if param.grad is None:
                     continue
                 grad = param.grad.data
+                grad.add_(param.data, alpha=self.weight_decay)
+
 
                 m.mul_(self.beta1).add_(grad, alpha=1 - self.beta1)
                 v.mul_(self.beta2).addcmul_(grad, grad, value=1 - self.beta2)
